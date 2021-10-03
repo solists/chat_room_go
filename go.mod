5c344489723b61f1dc7a9c823f7e9369416240b2
Solomennikov Semen <999solom.s.v.999@gmail.com>
2021-10-03 16:15:59 +0300

    Added template for clickhouse db & adapter

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module chat_room_go
 go 1.16
 
 require (
+	github.com/ClickHouse/clickhouse-go v1.4.8 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 )
---
+++ microservices/clickhouse/adapter.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	connect, err := sqlx.Open("clickhouse", "tcp://127.0.0.1:9000?debug=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := connect.Ping(); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		} else {
+			fmt.Println(err)
+		}
+		return
+	}
+
+	_, err = connect.Exec(`
+		CREATE TABLE IF NOT EXISTS example (
+			country_code FixedString(2),
+			os_id        UInt8,
+			browser_id   UInt8,
+			categories   Array(Int16),
+			action_day   Date,
+			action_time  DateTime
+		) engine=Memory
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var (
+		tx, _   = connect.Begin()
+		stmt, _ = tx.Prepare("INSERT INTO example (country_code, os_id, browser_id, categories, action_day, action_time) VALUES (?, ?, ?, ?, ?, ?)")
+	)
+	defer stmt.Close()
+
+	for i := 0; i < 100; i++ {
+		if _, err := stmt.Exec(
+			"RU",
+			10+i,
+			100+i,
+			clickhouse.Array([]int16{1, 2, 3}),
+			time.Now(),
+			time.Now(),
+		); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+
+	var items []struct {
+		CountryCode string    `db:"country_code"`
+		OsID        uint8     `db:"os_id"`
+		BrowserID   uint8     `db:"browser_id"`
+		Categories  []int16   `db:"categories"`
+		ActionTime  time.Time `db:"action_time"`
+	}
+
+	if err := connect.Select(&items, "SELECT country_code, os_id, browser_id, categories, action_time FROM example"); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, item := range items {
+		log.Printf("country: %s, os: %d, browser: %d, categories: %v, action_time: %s", item.CountryCode, item.OsID, item.BrowserID, item.Categories, item.ActionTime)
+	}
+}
